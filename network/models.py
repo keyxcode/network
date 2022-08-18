463ac8759c232af7b438f779125d1081bb3e7a8e
keyxcode <keyxcode@gmail.com>
2022-08-18 18:39:41 +0700

    implement posts

--- network/models.py
+++ network/models.py
@@ -4,3 +4,11 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Post(models.Model):
+    content = models.CharField(max_length=256)
+    poster = models.ForeignKey(User, on_delete=models.CASCADE, null=True, related_name="all_posts")
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.poster}: {self.content}"
--- network/views.py
+++ network/views.py
@@ -4,11 +4,15 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-from .models import User
+from .models import *
 
 
 def index(request):
-    return render(request, "network/index.html")
+    posts = Post.objects.all().order_by('-timestamp')
+    
+    return render(request, "network/index.html", {
+        "posts": posts
+    })
 
 
 def login_view(request):
@@ -61,3 +65,19 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "network/register.html")
+
+
+def create_post(request):
+    if request.method == "POST":
+        content = request.POST['content']
+        poster = request.user
+
+        new_post = Post.objects.create(
+            content = content,
+            poster = poster
+        )
+        new_post.save()
+
+    return HttpResponseRedirect(reverse("index"))
+
+
